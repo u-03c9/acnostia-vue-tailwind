6554db2df17b74d5538b91390b6d59dda73e1b77
Peter Khalil <uni.03c9@gmail.com>
2022-03-08 01:59:53 +0200

    footer fix

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -7,12 +7,27 @@ import LinkedinIcon from "../assets/footer/linkedin.webp";
 
 <template>
   <div class="w-full bg-[#68a097] py-10">
-    <div class="container w-1/4 flex flex-row mx-auto justify-between">
-      <img class="hover:opacity-90 cursor-pointer" :src="FacebookIcon" alt="facebook" />
-      <img class="hover:opacity-90 cursor-pointer" :src="TwitterIcon" alt="twitter" />
-      <img class="hover:opacity-90 cursor-pointer" :src="GoogleIcon" alt="google" />
-      <img class="hover:opacity-90 cursor-pointer" :src="LinkedinIcon" alt="linkedin" />
+    <div class="w-fit flex flex-row mx-auto justify-between gap-10">
+      <img
+        class="hover:opacity-90 cursor-pointer"
+        :src="FacebookIcon"
+        alt="facebook"
+      />
+      <img
+        class="hover:opacity-90 cursor-pointer"
+        :src="TwitterIcon"
+        alt="twitter"
+      />
+      <img
+        class="hover:opacity-90 cursor-pointer"
+        :src="GoogleIcon"
+        alt="google"
+      />
+      <img
+        class="hover:opacity-90 cursor-pointer"
+        :src="LinkedinIcon"
+        alt="linkedin"
+      />
     </div>
   </div>
 </template>
-
